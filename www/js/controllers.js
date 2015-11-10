29862c0a9e69b75fa771c1cd0b13ad995979aa8c
muratrl <murat@rentlever.com>
2015-11-09 17:44:34 -0800

    added last piece

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -55,13 +55,53 @@ angular.module('starter.controllers', [])
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 })
 .controller('DashCtrl',function($scope,Chats){
-  $scope.chats = [];
+    $scope.chats = [];
 
     var handleSuccess = function(data, status) {
         $scope.chats = data;
         console.log($scope.chats);
     };
-   Chats.getEvents().success(handleSuccess);
+    Chats.getEvents().success(handleSuccess);
+    //date picker
+    $scope.datepickerObject = {
+      titleLabel: 'Title',  //Optional
+      todayLabel: 'Today',  //Optional
+      closeLabel: 'Close',  //Optional
+      setLabel: 'Set',  //Optional
+      setButtonType : 'button-assertive',  //Optional
+      todayButtonType : 'button-assertive',  //Optional
+      closeButtonType : 'button-assertive',  //Optional
+      inputDate: new Date(),  //Optional
+      mondayFirst: true,  //Optional
+      disabledDates: disabledDates, //Optional
+      weekDaysList: weekDaysList, //Optional
+      monthList: monthList, //Optional
+      templateType: 'popup', //Optional
+      showTodayButton: 'true', //Optional
+      modalHeaderColor: 'bar-positive', //Optional
+      modalFooterColor: 'bar-positive', //Optional
+      from: new Date(2012, 8, 2), //Optional
+      to: new Date(2018, 8, 25),  //Optional
+      callback: function (val) {  //Mandatory
+        datePickerCallback(val);
+      },
+      dateFormat: 'dd-MM-yyyy', //Optional
+      closeOnSelect: false, //Optional
+    };
+    var monthList = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+    var weekDaysList = ["Sun", "Mon", "Tue", "Wed", "thu", "Fri", "Sat"];
+    var datePickerCallback = function (val) {
+      if (typeof(val) === 'undefined') {
+        console.log('No date selected');
+      } else {
+        console.log('Selected date is : ', val)
+        //set the date to the selected date
+        $scope.datepickerObject.inputDate=val;
+      }
+    };
+    var disabledDates = [];
+    // console.log($scope.datepickerObject.inputDate)
+
 })
 .controller('ChatsCtrl', function($scope) {
   // With the new view caching in Ionic, Controllers are only called
